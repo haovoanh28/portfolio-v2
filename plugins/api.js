1467f13a201edaa269a17b3928b72060ca3e0766
haovoanh28 <haovoanh28@gmail.com>
2021-05-04 22:34:08 +0700

    [improve] improve baseUrl selection

--- plugins/api.js
+++ plugins/api.js
@@ -1,10 +1,18 @@
 export default function ({ $axios, store }, inject) {
-  const baseURL = process.env.BASE_URL
+  const baseURL = ''
+
+  if (process.env.NODE_ENV === 'development') {
+    baseURL = process.env.BASE_URL_DEV
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    baseURL = process.env.BASE_URL_PRODUCTION
+  }
 
   const api = $axios.create({ baseURL: baseURL })
   api.interceptors.request.use(
     (config) => {
-        // const token = store.state.auth.accessToken ?? ''
+      // const token = store.state.auth.accessToken ?? ''
       // if (token) config.headers.Authorization = `Bearer ${token}`
 
       return config
