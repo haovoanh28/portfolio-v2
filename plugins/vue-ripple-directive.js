fa610f5455d763c74d5ccef9cd0b5cae2815440d
haovoanh28 <haovoanh28@gmail.com>
2021-05-20 22:59:09 +0700

    [Common] do some stytling

--- plugins/vue-ripple-directive.js
+++ plugins/vue-ripple-directive.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Ripple from 'vue-ripple-directive'
 
-Ripple.color = 'rgba(236, 236, 236, 0.5)'
+Ripple.color = 'rgba(255, 255, 255, 0.7)'
 Ripple.zIndex = 55
 
 Vue.directive('ripple', Ripple)
